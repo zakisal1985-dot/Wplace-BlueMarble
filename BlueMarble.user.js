224b786dd49cc9830b8b6b22d337d391ae766d80
SwingTheVine <swingthevine@gmail.com>
2025-07-23 18:40:55 -0400

    Initial Commit

---
+++ BlueMarble.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Blue Marble
+// @namespace    https://github.com/SwingTheVine/
+// @version      0.0.1
+// @description  A userscript for Wplace.live
+// @author       SwingTheVine
+// @license      MPL-2.0
+// @supportURL   https://discord.gg/tpeBPy46hf
+// @homepageURL  https://github.com/SwingTheVine/Wplace-BlueMarble
+// @icon         https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/Favicon.png
+// @updateURL    https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/BlueMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/BlueMarble.user.js
+// @match        *://*.wplace.live/*
+// ==/UserScript==
+
+/** This script is designed to automate and/or enhance the user experience on Wplace.live.
+ * Make sure to comply with the site's Terms of Service, and rules!
+ * This script is not affiliated with Wplace.live in any way.
+ * Use at your own risk.
+ * The author is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script.
+ * This script is provided "as is" under the MPL-2.0 license.
+ * The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
+ * @author SwingTheVine
+ * @version 0.0.1
+ * @description A userscript for Wplace.live
+ * @license MPL-2.0
+ * @link https://wplace.live
+ * @link https://github.com/SwingTheVine/Wplace-BlueMarble
+ * @link https://www.mozilla.org/en-US/MPL/2.0/
+*/ 
+
+console.log("Blue Marble script loaded.");
